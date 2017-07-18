c6e4e1de17ce3144b0969476c2b1fd68b9ba91b0
searobbersduck <searobbersandduck@gmail.com>
2017-07-18 17:04:24 +0800

    commit

--- data/train_validation_set_generator.py
+++ data/train_validation_set_generator.py
@@ -61,14 +61,41 @@ for label in range(5):
     tr = tr_list[label]
     te = te_list[label]
     for tr_img in tr:
-        shutil.copy(os.path.join(args.root, tr_img+'.jpg'), train_dir)
+        raw = os.path.join(args.root, tr_img + '.jpg')
+        raw_128 = os.path.join(args.root, tr_img + '_128.png')
+        raw_256 = os.path.join(args.root, tr_img + '_256.png')
+        raw_512 = os.path.join(args.root, tr_img + '_512.png')
+        raw_1024 = os.path.join(args.root, tr_img + '_1024.png')
+        raw_list = []
+        raw_list.append(raw)
+        raw_list.append(raw_128)
+        raw_list.append(raw_256)
+        raw_list.append(raw_512)
+        raw_list.append(raw_1024)
+        for r in raw_list:
+            if os.path.exists(r):
+                shutil.copy(r, train_dir)
         f1.write(tr_img+'\r')
         f2.write(str(label)+'\r')
 
     for te_img in te:
-        shutil.copy(os.path.join(args.root, te_img+'.jpg'), val_dir)
+        raw = os.path.join(args.root, te_img + '.jpg')
+        raw_128 = os.path.join(args.root, te_img + '_128.png')
+        raw_256 = os.path.join(args.root, te_img + '_256.png')
+        raw_512 = os.path.join(args.root, te_img + '_512.png')
+        raw_1024 = os.path.join(args.root, te_img + '_1024.png')
+        raw_list = []
+        raw_list.append(raw)
+        raw_list.append(raw_128)
+        raw_list.append(raw_256)
+        raw_list.append(raw_512)
+        raw_list.append(raw_1024)
+        if r in raw_list:
+            if os.path.exists(r):
+                shutil.copy(r, val_dir)
         f3.write(te_img+'\r')
         f4.write(str(label)+'\r')
+
 
 
 
