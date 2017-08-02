51ffffb712ff2d4d534d160de2be6a8da29e5733
searobbersduck <searobbersandduck@gmail.com>
2017-08-02 20:56:06 +0800

    commit

---
+++ image_processing_test/copy_kaggle_train_to_all.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+from glob import glob
+import shutil
+
+parser = argparse.ArgumentParser(description='generate train & validation set')
+
+parser.add_argument('--root', required=True)
+parser.add_argument('--testratio', type=float, default=0.1)
+
+args = parser.parse_args()
+
+root = args.root
+
+train_path = os.path.join(root, 'train')
+all_path = os.path.join(root, 'all')
+
+src_list = glob(os.path.join(train_path, '*.png'))
+
+for index in src_list:
+    shutil.copy(index, all_path)
+    print('copy from {0} to {1}'.format(index, all_path))
