05dc02ca4c4093c5aa958034c9c70b18905df341
searobbersduck <searobbersandduck@gmail.com>
2017-09-01 10:52:43 +0800

    commit

---
+++ paper_data/task_train.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 python multi_task_classification.py --root ../data/dme/512 --traincsv ../data/dme/train.csv --valcsv ../data/dme/val.csv --testcsv ../data/dme/test.csv --model rsn18 --batch 200
+CUDA_VISIBLE_DEVICES=0,1,2,3 python multi_task_classification.py --root ../data/dme/512 --traincsv ../data/dme/train.csv --valcsv ../data/dme/val.csv --testcsv ../data/dme/test.csv --model rsn34 --batch 200
+CUDA_VISIBLE_DEVICES=0,1,2,3 python multi_task_classification.py --root ../data/dme/512 --traincsv ../data/dme/train.csv --valcsv ../data/dme/val.csv --testcsv ../data/dme/test.csv --model rsn50 --batch 64
+CUDA_VISIBLE_DEVICES=0,1,2,3 python multi_task_classification.py --root ../data/dme/512 --traincsv ../data/dme/train.csv --valcsv ../data/dme/val.csv --testcsv ../data/dme/test.csv --model rsn101 --batch 32
+CUDA_VISIBLE_DEVICES=0,1,2,3 python multi_task_classification.py --root ../data/dme/512 --traincsv ../data/dme/train.csv --valcsv ../data/dme/val.csv --testcsv ../data/dme/test.csv --model dsn121 --batch 32
+
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python multi_channel_classification.py --root ../data/dme/512 --root_augumentation ../data/dme/512_dme --traincsv ../data/dme/train.csv --valcsv ../data/dme/val.csv --testcsv ../data/dme/test.csv --model rsn18 --batch 100
+CUDA_VISIBLE_DEVICES=0,1,2,3 python multi_channel_classification.py --root ../data/dme/512 --root_augumentation ../data/dme/512_dme --traincsv ../data/dme/train.csv --valcsv ../data/dme/val.csv --testcsv ../data/dme/test.csv --model rsn34 --batch 100
+CUDA_VISIBLE_DEVICES=0,1,2,3 python multi_channel_classification.py --root ../data/dme/512 --root_augumentation ../data/dme/512_dme --traincsv ../data/dme/train.csv --valcsv ../data/dme/val.csv --testcsv ../data/dme/test.csv --model rsn50 --batch 64
+CUDA_VISIBLE_DEVICES=0,1,2,3 python multi_channel_classification.py --root ../data/dme/512 --root_augumentation ../data/dme/512_dme --traincsv ../data/dme/train.csv --valcsv ../data/dme/val.csv --testcsv ../data/dme/test.csv --model rsn101 --batch 16
+CUDA_VISIBLE_DEVICES=0,1,2,3 python multi_channel_classification.py --root ../data/dme/512 --root_augumentation ../data/dme/512_dme --traincsv ../data/dme/train.csv --valcsv ../data/dme/val.csv --testcsv ../data/dme/test.csv --model dsn121 --batch 16
+
+
