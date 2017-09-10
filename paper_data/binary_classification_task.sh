d9059f2adf123d974dd9e2c47ee7f3a5fb8bea5e
searobbersduck <searobbersandduck@gmail.com>
2017-09-10 17:15:19 +0800

    commit

--- paper_data/binary_classification_task.sh
+++ paper_data/binary_classification_task.sh
@@ -23,4 +23,13 @@
 
 #CUDA_VISIBLE_DEVICES=0,1 python binary_classification.py --root ../data/tmp/512 --traincsv ../data/tmp/train_bin.csv --valcsv ../data/tmp/val_bin.csv --testcsv ../data/tmp/test_bin.csv --workers 4 --batch 64 --epoch 30 --model rsn18 --phase test --weight /home/weidong/code/dr/DiabeticRetinopathy_solution/paper_data/output/kaggle_binary_cls_train_20170908084549_rsn18_binary_cls/kaggle_binarycls_rsn18_000_best.pth
 
-CUDA_VISIBLE_DEVICES=0,1 python binary_classification.py --root ../data/zhizhen_new/LabelImages/512 --traincsv ../data/zhizhen_new/LabelImages/val_multi.csv --valcsv ../data/zhizhen_new/LabelImages/val_multi.csv --testcsv ../data/zhizhen_new/LabelImages/test_bin.csv --workers 4 --batch 64 --epoch 30 --model rsn18
+#CUDA_VISIBLE_DEVICES=0,1 python binary_classification.py --root ../data/zhizhen_new/LabelImages/512 --traincsv ../data/zhizhen_new/LabelImages/val_multi.csv --valcsv ../data/zhizhen_new/LabelImages/val_multi.csv --testcsv ../data/zhizhen_new/LabelImages/test_bin.csv --workers 4 --batch 64 --epoch 30 --model rsn18
+
+
+
+
+### GPU server
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python binary_classification.py --root ../data/zhizhen_new/LabelImages/512 --traincsv ../data/zhizhen_new/LabelImages/val_multi.csv --valcsv ../data/zhizhen_new/LabelImages/val_multi.csv --testcsv ../data/zhizhen_new/LabelImages/test_bin.csv --workers 4 --batch 64 --epoch 30 --model rsn34
+CUDA_VISIBLE_DEVICES=0,1,2,3 python binary_classification.py --root ../data/zhizhen_new/LabelImages/512 --traincsv ../data/zhizhen_new/LabelImages/val_multi.csv --valcsv ../data/zhizhen_new/LabelImages/val_multi.csv --testcsv ../data/zhizhen_new/LabelImages/test_bin.csv --workers 4 --batch 64 --epoch 30
+CUDA_VISIBLE_DEVICES=0,1,2,3 python binary_classification.py --root ../data/zhizhen_new/LabelImages/512 --traincsv ../data/zhizhen_new/LabelImages/val_multi.csv --valcsv ../data/zhizhen_new/LabelImages/val_multi.csv --testcsv ../data/zhizhen_new/LabelImages/test_bin.csv --workers 4 --batch 16 --epoch 30 --model dsn121
